ff37cb7182bd80bf9b37b53319f68e12a03e6863
SaravananLal <l.saravanan92@gmail.com>
2023-02-02 09:56:22 +0530

    UI Codes

---
+++ wecareproject/src/components/Home.js
@@ -0,0 +1,26 @@
+const Home = () => {
+  return (
+    <>
+    <div className="header">
+        <h1>WeCare <p1> CallUs : 044 27283456</p1></h1>
+
+    </div>
+       <div className="container">
+        <div className="card">
+          <image src=""/>
+          <input type="button" value="Login as Coach" />
+          <br/>
+          <input type="button" value="Join as Coach" />
+
+        </div>
+        <div className="card">
+        <input type="button" value="Login as User" />
+          <br/>
+          <input type="button" value="Join as User" />
+        </div>
+      </div>
+      <div className="footer">CopyRight @ 2020,WeCare. All rights reserved</div>
+</>
+  );
+};
+export default Home;
